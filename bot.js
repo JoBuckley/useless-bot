66df43e54b464e9cd7edb6fee196ce135f878bbb
JoBuckley <47748284+JoBuckley@users.noreply.github.com>
2019-02-18 14:44:55 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 function answerQuery(query) {
   query = query.toLowerCase();
-  if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
-    return "I do not care too much about weather, I'm locked inside a data center.";
+  if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1 || query.indexOf("vensday") != -1)
+      return "I do not care too much about weather, I'm locked inside a data center.";
+  return "Vensday is messy";
   return "Sorry Dave, I cannot do that."
 }
 
